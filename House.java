c26ea325f6378d713aaa33fbb1fe802c94fbd111
Tillie Slosser <tillie@slosser.net>
2024-03-08 10:11:27 -0500

    Finish Library + build House (no javadoc yet)

--- House.java
+++ House.java
@@ -1,12 +1,69 @@
-/* This is a stub for the House class */
-public class House {
+import java.util.ArrayList;
+import java.util.Arrays;
 
-  public House() {
+public class House extends Building {
+
+  private ArrayList<String> residents; // The <String> tells Java what kind of data we plan to store IN the ArrayList
+  private boolean hasDiningRoom;
+  private boolean hasElevator;
+
+  public House(String name, String address, int nFloors, ArrayList<String> residents, boolean hasDiningRoom, boolean hasElevator) {
+    super(name, address, nFloors);
+    this.residents = residents;
+    this.hasDiningRoom = hasDiningRoom;
+    this.hasElevator = hasElevator;
     System.out.println("You have built a house: 🏠");
   }
+  public String toString() {
+    String desc = super.toString();
+    // source for inline if statement:
+    // https://www.delftstack.com/howto/java/one-line-if-statement-java/
+    String elev = this.hasElevator ? "has an elevator" : "does not have an elevator";
+    String dining = this.hasDiningRoom ? "has a dining hall" : "does not have a dining hall";
+    return "Residence Hall: " + desc + ". It " + elev + " and " + dining + "."; 
+  }
+  public boolean hasDiningRoom() {
+    return this.hasDiningRoom;
+  }
+
+  public int nResidents() {
+    return this.residents.size();
+  }
+
+  public void moveIn(String name) {
+    this.residents.add(name);
+  }
+
+  public String moveOut(String name) {
+    if (this.residents.contains(name)) {
+      this.residents.remove(name);
+      return name + " has moved out.";
+    } else {
+      return name + " could not move out because they do not live in " + this.name;
+    }
+    
+  }
+
+  public boolean isResident(String person) {
+    return this.residents.contains(person);
+  }
+
 
   public static void main(String[] args) {
-    new House();
+    // Make some residents
+    ArrayList<String> gillettResidents = new ArrayList<String>(Arrays.asList("Nina Wattenberg", "Elm Markert", "Tillie Slosser", "Taylor Agena", "Em Swindell", "Ava Silverman", "Alex Theiss"));
+    // Make my house
+    House gillett = new House("Gillett House", "47 Elm Street", 5, gillettResidents, true, true);
+    System.out.println(gillett);
+    // How many residents does it have?
+    System.out.println("Gillett has " + gillett.nResidents() + " residents.");
+    // Am I a resident?
+    System.out.println(gillett.isResident("Tillie Slosser"));
+    // The president is graduating this year  - move her out. 
+    System.out.println(gillett.moveOut("Nina Wattenberg"));
+    // Try to move her out a second time to test the "person not a resident" scenario
+    System.out.println(gillett.moveOut("Nina Wattenberg"));
+    
   }
 
 }
--- Library.java
+++ Library.java
@@ -1,12 +1,180 @@
 /* This is a stub for the Library class */
-public class Library {
 
-    public Library() {
-      System.out.println("You have built a library: 📖");
-    }
-  
-    public static void main(String[] args) {
-      new Library();
-    }
-  
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.TreeMap;
+
+import javax.naming.InvalidNameException;
+import javax.naming.LimitExceededException;
+
+public class Library extends Building {
+
+  private Hashtable<String, Boolean> collection;
+
+  /**
+   * Constructor for the Library Class. This class is an extension of the
+   * Building() Class
+   * 
+   * @param name    the name of the library building to construct, passed to the
+   *                parent class
+   * @param address the address of the building, passed to the parent class
+   * @param nFloors the number of floors the building has
+   */
+  public Library(String name, String address, int nFloors) {
+    super(name, address, nFloors);
+    this.collection = new Hashtable<>();
+    System.out.println("You have built a library: 📖");
   }
+
+  /**
+   * Add a title to the library's collection. If the title already exists, this
+   * function prints a statement warning the user.
+   * 
+   * @param title the book to add
+   */
+  public void addTitle(String title) {
+    try {
+      // If the book is already in the collection, start search for lowest unique copy
+      // number
+      if (this.collection.containsKey(title)) {
+        for (int i = 1; i < 200; i++) {
+          // If the current iteration of for loop is not a current copy, re-assign title
+          // to include copy info
+          if (!this.collection.containsKey(title + ", copy " + i)) {
+            title = title + ", copy " + i;
+            break;
+          }
+          // If on the last iteration, throw an error to let user know that there are too
+          // many books with that title
+          if (i == 199) {
+            throw new LimitExceededException();
+          }
+        } // exit for loop
+      } // exit check for existing copy
+      // add the title (may have been edited to include copy num!)
+      this.collection.put(title, true);
+      System.out.println("TITLE ADDED: " + title);
+    } catch (Exception e) {
+      System.out.println(
+          "Error in adding " + title + ". There are too many copies of this book, please add a different title.");
+    }
+  }
+
+  /**
+   * Removes a title from the collection (as opposed to checking it out)
+   * 
+   * @param title the title that is to be removed
+   * @return the removed title
+   */
+  public String removeTitle(String title) {
+    try {
+      if (this.collection.containsKey(title)) {
+        this.collection.remove(title, true);
+        return (title);
+      } else {
+        throw new InvalidNameException("The title \"" + title + "\" is not in the library, and cannot be removed.");
+      }
+    } catch (Exception e) {
+      System.out.println(e);
+      return (title);
+    }
+  }
+
+  /**
+   * Checks a title out of the collection (as opposed to removing it entirely)
+   * 
+   * @param title the title to be checked out
+   */
+  public void checkOut(String title) {
+    try {
+      if (this.collection.containsKey(title)) {
+        this.collection.replace(title, false);
+      } else {
+        throw new InvalidNameException("The title \"" + title + "\" is not in the library, and cannot be checked out.");
+      }
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+  }
+
+  /**
+   * Returns a book to the library (as opposed to adding a new copy)
+   * 
+   * @param title the title to be returned
+   */
+  public void returnBook(String title) {
+    try {
+      if (this.collection.containsKey(title)) {
+        this.collection.replace(title, true);
+      } else {
+        throw new InvalidNameException("The title \"" + title + "\" is not in the library, and cannot be returned.");
+      }
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+  }
+
+  /**
+   * Returns true if the title appears as a key in the Libary's collection, false
+   * otherwise
+   * 
+   * @param title the title in question
+   * @return a true or false value based on if the title is present in the
+   *         collection
+   */
+  public boolean containsTitle(String title) {
+    return (this.collection.containsKey(title));
+  }
+
+  /**
+   * Returns true if the title is currently available, false otherwise. Throws and
+   * catches an error if the title is not in the collection at all.
+   * 
+   * @param title the title in question
+   * @return a true or false value based on if the title is currently availible
+   */
+  public boolean isAvailable(String title) {
+    try {
+      if (containsTitle(title)) {
+        return (this.collection.getOrDefault(title, false));
+      } else {
+        throw new InvalidNameException(
+            "The title \"" + title + "\" is not in the library, therefore it is not available.");
+      }
+    } catch (Exception e) {
+      System.out.println(e);
+      return (false);
+    }
+  }
+
+  /**
+   * prints out the entire collection in an easy-to-read way (including checkout
+   * status)
+   */
+  public void printCollection() {
+    // source for Treemap inspiration: https://www.javacodeexamples.com/sort-hashtable-by-keys-in-java-example/3165
+    Map<String, Boolean> sortedCollection = new TreeMap<String, Boolean>(this.collection);
+    System.out.println("\n------Welcome to " + this.name + " Library. Here is our selection:------");
+    sortedCollection.forEach(
+        (title, checkout_status) -> {
+          String checkout_str = checkout_status ? "available" : "checked out";
+          System.out.println("\tThe book " + title + " is " + checkout_str);
+        });
+  }
+
+  public static void main(String[] args) {
+    Library josten = new Library("Josten", "122 Green Street", 3);
+    josten.addTitle("Music Theory I by B. Toven");
+    josten.addTitle("Music Theory I by B. Toven");
+    josten.addTitle("Rock 'n Roll Classics by Elle Vess.");
+    josten.addTitle("Whaling Songs of the Northeast Atlantic, 1840-1970 by C. Shantiés");
+
+    josten.checkOut("Music Theory I by B. Toven");
+    josten.printCollection();
+
+    josten.returnBook("Music Theory I by B. Toven");
+    josten.returnBook("Cats and The Ice Age");
+    josten.printCollection();
+  }
+
+}
